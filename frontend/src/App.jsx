3e50a479434d41f0ac5af8a93362a4c6c0ab9b88
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2025-11-22 11:29:13 -0500

    fixed some of the styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import PaymentSuccess from './pages/PaymentSuccess.jsx'
-import PaymentCancelled from './pages/PaymentCancelled.jsx'
+// src/App.jsx
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import PaymentSuccess from "./pages/PaymentSuccess.jsx";
+import PaymentCancelled from "./pages/PaymentCancelled.jsx";
+import "./styles/App.css"; // ⬅️ new
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/payment-success" element={<PaymentSuccess />} />
-        <Route path="/payment-cancelled" element={<PaymentCancelled />} />
-      </Routes>
+      <div className="app-root">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/payment-success" element={<PaymentSuccess />} />
+          <Route path="/payment-cancelled" element={<PaymentCancelled />} />
+        </Routes>
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
