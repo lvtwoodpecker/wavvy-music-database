31e258b502df1d32cb6ae7ed5ba70a561d8ab233
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2025-12-21 00:48:57 -0500

    change order of imports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,18 +1,18 @@
-from app.models.Advertiser import Advertiser
-from app.models.Listener import Listener
-from app.models.StripeAccount import StripeAccount
 from app.models.User import User
+from app.models. Advertiser import Advertiser
+from app.models. Listener import Listener
+from app.models.StripeAccount import StripeAccount
 from app.models.Track import Track
-from app.models.Playlist import Playlist
-from app.models.PlaylistTrack import PlaylistTrack
+from app.models. Playlist import Playlist
+from app. models.PlaylistTrack import PlaylistTrack
 from app.models.PlayHistory import PlayHistory
 from app.models.SubscriptionHistory import SubscriptionHistory
 from app.models.SubscriptionPlan import SubscriptionPlan
 from app.models.SubscriptionPlanPrice import SubscriptionPlanPrice
 from app.models.ODSTrackSearch import ODSTrackSearch
-from app.models.Competitor import Competitor
-from app.models.CompetitorAdProduct import CompetitorAdProduct
-from app.models.CompetitorAdRateSnapshot import CompetitorAdRateSnapshot
+from app.models. Competitor import Competitor
+from app.models.CompetitorAdProduct import CompetitorAdProduct  # Import this BEFORE it's referenced
+from app.models. CompetitorAdRateSnapshot import CompetitorAdRateSnapshot
 from app.models.CompetitorSubscriptionPlan import CompetitorSubscriptionPlan
 from app.models.CompetitorSubscriptionPriceSnapshot import CompetitorSubscriptionPriceSnapshot
 
